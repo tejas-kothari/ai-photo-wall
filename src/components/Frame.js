adeb6c2b5fb9b374aff0df90ea724e3176bca886
edward21 <edward210400@gmail.com>
2021-08-20 19:44:27 +0800

    Results Page update

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Frame.css";
 import "styles.css";
-import FrameButton from "components/FrameButton";
 
 export default function Frame(props){
   const [image, setImage] = useState(null);
@@ -10,16 +9,22 @@ export default function Frame(props){
     if (event.target.files && event.target.files[0]) {
     	let img = event.target.files[0];
       setImage(URL.createObjectURL(img))
+			console.log(props.img)
     }
   };
+
   return(
     <div className="frame-container">
     	<div className={"Frame " + props.className} style={props.style}>
-      	<img className="image" src={props.img}/>
+				<label htmlFor={props.identifier} className="upload-label">
+					{
+						"Upload" 
+					} 
+				</label>
+      	<img className="image" src={image===null ? props.img : image}/>
       	<br/>
     	</div>
-			<label for={props.identifier} className="upload-label" style={{}}> Upload </label>
-    	<input type="file" id={props.identifier} className="upload-button" onChange={() => onImageChange}/>
+    	<input type="file" id={props.identifier} className="upload-button" onChange={(e) => onImageChange(e)}/>
     </div>
   )
 }
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -6,101 +6,97 @@ import FrameButton from "components/FrameButton";
 import ActionButton from "components/ActionButton";
 import { useLocation } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import Draggable from 'react-draggable';
+import Draggable from "react-draggable";
 
-export default function ResultsPage(){
-	const location = useLocation();
-	let history = useHistory();
+export default function ResultsPage() {
+  const location = useLocation();
+  let history = useHistory();
 
-	const [frameArray1, setframeArray1] = useState(location.state.layout);
+  const [frameArray1, setframeArray1] = useState(location.state.layout);
 
-	const handleAddFrame = (e) => {
-		let frameArray = [...frameArray1]
-		var key = e.target.id
-		var temp = frameArray[key]
-		console.log(key)
-		console.log(temp)
-		frameArray.push(temp)
-		console.log(frameArray)
-		setframeArray1(frameArray);
-	}
+  const handleAddFrame = (e) => {
+    let frameArray = [...frameArray1];
+    var key = e.target.id;
+    var temp = frameArray[key];
+    frameArray.push(temp);
+    setframeArray1(frameArray);
+  };
 
-	const handleDeleteFrame = (e) => {
-		let frameArray = [...frameArray1]
-		console.log('ss')
-		var key = e.target.id
-		console.log("key: "+key)
-		if (key !== -1) {
-			console.log(key)
-			frameArray.splice(key, 1);
-			setframeArray1(frameArray);
-		}
-		console.log(frameArray)
-	}
+  const handleDeleteFrame = (e) => {
+    let frameArray = [...frameArray1];
+    var key = e.target.id;
+    if (key !== -1) {
+      frameArray.splice(key, 1);
+      setframeArray1(frameArray);
+    }
+  };
 
-
-  return(
-		<div className="ResultsPage">
-			<h1 className="PageTitle">Your AI Wall</h1>
-			<div className="infoContainer">
-				<div style={{width:80}}>
-					<h3 className="infoTitle"> Wall Size </h3>
-					<h3 className="infoContent"> {location.state.sizeVal} </h3>
-				</div>
-				<div style={{width:80}}>
-					<h3 className="infoTitle"> Scale </h3>
-					<h3 className="infoContent"> {location.state.scale} </h3>
-				</div>
-			</div>
-			<div 
-				className="frameRoomContainer" 
-			>
-				<img src={location.state.wallImage} className="roomImg" />
-				<div
-					className="frameArea"
-					style={{ width: location.state.areaWidth + "%" }}
-				>
-					<div className="framesContainer">
-						{frameArray1.map((frameObj, index) => (
-							<Draggable>
-								<div 
-								key={index}
-								style={{
-									top : frameObj.top ? frameObj.top : "auto",
-								}}>
-									<FrameButton 
-										id={index} 
-										marginLeft={frameObj.left + frameObj.width/2}  
-										top={frameObj.top ? frameObj.top : "auto"} 
-										onClickAdd={(e) => handleAddFrame(e)} 
-										onClickDelete={(e) => handleDeleteFrame(e)}/>
-									<Frame
-										identifier={"frame-"+index}
-										className="framePos"
-										lineHeight={location.state.frameAreaWidth * frameObj.width * frameObj.ratio}
-										style={{
-											width: frameObj.width * 100 + "%",
-											height:
-												location.state.frameAreaWidth * frameObj.width * frameObj.ratio,
-											left: frameObj.left
-												? frameObj.left * 100 + "%"
-												: "auto",
-											top: frameObj.top ? frameObj.top : "auto",
-										}}
-									/>
-								</div>
-							</Draggable>
-						))}
-					</div>
-				</div>
-			</div>
-			<ActionButton
+  return (
+    <div className="ResultsPage">
+      <h1 className="PageTitle">Your AI Wall</h1>
+      <div className="infoContainer">
+        <div style={{ width: 80 }}>
+          <h3 className="infoTitle"> Wall Size </h3>
+          <h3 className="infoContent"> {location.state.sizeVal} </h3>
+        </div>
+        <div style={{ width: 80 }}>
+          <h3 className="infoTitle"> Scale </h3>
+          <h3 className="infoContent"> {location.state.scale} </h3>
+        </div>
+      </div>
+      <div className="frameRoomContainer">
+        <img src={location.state.wallImage} className="roomImg" />
+        <div
+          className="frameArea"
+          style={{ width: location.state.areaWidth + "%" }}
+        >
+          <div className="framesContainer">
+            {frameArray1.map((frameObj, index) => (
+              <Draggable>
+                <div
+                  key={index}
+                  style={{
+                    top: frameObj.top ? frameObj.top : "auto",
+                  }}
+                >
+                  <FrameButton
+                    id={index}
+                    marginLeft={frameObj.left + frameObj.width / 2}
+                    top={frameObj.top ? frameObj.top : "auto"}
+                    onClickAdd={(e) => handleAddFrame(e)}
+                    onClickDelete={(e) => handleDeleteFrame(e)}
+                  />
+                  <Frame
+                    identifier={"frame-" + index}
+                    className="framePos"
+                    lineHeight={
+                      location.state.frameAreaWidth *
+                      frameObj.width *
+                      frameObj.ratio
+                    }
+                    style={{
+                      width: frameObj.width * 100 + "%",
+                      height:
+                        location.state.frameAreaWidth *
+                        frameObj.width *
+                        frameObj.ratio,
+                      left: frameObj.left ? frameObj.left * 100 + "%" : "auto",
+                      top: frameObj.top ? frameObj.top : "auto",
+                    }}
+                  />
+                </div>
+              </Draggable>
+            ))}
+          </div>
+        </div>
+      </div>
+      <ActionButton
         clicked={false}
         caretLeft={true}
         onClick={() => history.push("/wall-area")}
       >
         Back
       </ActionButton>
-		</div>
-  )
+    </div>
+  );
 }
