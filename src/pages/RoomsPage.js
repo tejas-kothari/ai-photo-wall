b5f9131da5899263632798e3888e320e642afd0d
Tejas Kothari <tejas23682320@gmail.com>
2021-09-05 21:27:30 +0800

    Add custom wall image in RoomsPage

--- src/pages/RoomsPage.js
+++ src/pages/RoomsPage.js
@@ -5,11 +5,24 @@ import BubbleButton from "components/BubbleButton";
 import room1Img from "assets/rooms/1.png";
 import room2Img from "assets/rooms/2.png";
 import room3Img from "assets/rooms/3.png";
+import { ReactComponent as PlusIcon } from "assets/001-add.svg";
 import { useHistory } from "react-router-dom";
+import { useRef } from "react";
 
 export default function RoomsPage() {
   let history = useHistory();
   const [activatedBtn, setActivatedBtn] = useState(0);
+  const fileInputRef = useRef();
+  const [roomImgs, setRoomImgs] = useState([room1Img, room2Img, room3Img]);
+
+  const onImageAdd = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setRoomImgs((oldImgs) => [
+        URL.createObjectURL(event.target.files[0]),
+        ...oldImgs,
+      ]);
+    }
+  };
 
   return (
     <div className="RoomsPage standardPagePadding">
@@ -26,13 +39,17 @@ export default function RoomsPage() {
         ))}
       </div>
       <div className="imgList">
-        {[room1Img, room2Img, room3Img].map((roomImg) => (
+        {roomImgs.map((roomImg) => (
           <img
             className="roomImg"
             src={roomImg}
             onClick={() => history.push("/wall-area")}
           />
         ))}
+      </div>
+      <div className="addRoomImg" onClick={() => fileInputRef.current.click()}>
+        <PlusIcon className="plusIcon" />
+        <input type="file" hidden ref={fileInputRef} onChange={onImageAdd} />
       </div>
     </div>
   );
